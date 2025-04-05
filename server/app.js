276b6b47966af402f2339231d36b64559f8424f0
Yuvraj Mehta <yuvraj.mehta532@gmail.com>
2025-04-05 16:02:43 +0530

    book controller

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import {connectDB} from './database/database.js';   
 import { errorMiddleware } from './middlewares/errorMiddlewares.js';
 import authRouter from "./routes/authRouter.js"
+import bookRouter from "./routes/bookRouter.js"
 
 export const app = express();
 config({path: "config/config.env"});
@@ -23,6 +24,7 @@ app.use(express.urlencoded({extended: true}));
 
 
 app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/book", bookRouter)
 connectDB();
 
 app.use(errorMiddleware);
