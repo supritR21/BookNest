ccc9bf8f74f3679b729270f5fd84c71a18d272b7
Yuvraj Mehta <yuvraj.mehta532@gmail.com>
2025-04-05 16:12:13 +0530

    fixing merger issues

--- server/middlewares/authMiddlewares.js
+++ server/middlewares/authMiddlewares.js
@@ -1,15 +1,32 @@
 import { catchAsyncErrors } from "./catchAsyncErrors.js";
-import {errorMiddleware} from "./errorMiddlewares.js";
-
+import ErrorHandler from "./errorMiddlewares.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/userModel.js";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-    const token = req.cookies.token;
-    if(!token) {
-        return next(new ErrorHandler("User is not authenticated.", 400));
-    }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    req.user = await User.findById(decoded.id);
-    next();
+  // console.log("Checking authentication...");
+  
+  const { token } = req.cookies;
+  if (!token) {
+    return next(new ErrorHandler("User is not authenticated.", 400));
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);  
+  req.user = await User.findById(decoded.id);
+  next();
 });
+
+export const isAuthorized = (...roles) => {
+  
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role ${req.user.role} is not allowed to access this resource.`,
+          400
+        )
+      );
+    }
+    console.log("Checking authorization...");
+    next();
+  };
+}
